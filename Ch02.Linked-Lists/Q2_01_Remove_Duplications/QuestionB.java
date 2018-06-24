0e87a148612a5a8733b273d1bde73e1ea5c09003
cjh5414 <cjh5414@gmail.com>
2018-06-23 18:27:50 -0700

    Add Q2_01 QuestionB

--- Ch02.Linked-Lists/Q2_01_Remove_Duplications/QuestionB.java
+++ Ch02.Linked-Lists/Q2_01_Remove_Duplications/QuestionB.java
@@ -6,6 +6,19 @@ import LinkedListsLibrary.LinkedListNode;
 
 public class QuestionB {
     public static void removeDups(LinkedListNode temp) {
-        return;
+        LinkedListNode cur = temp;
+        LinkedListNode n;
+
+        while (cur != null) {
+            n = cur;
+            while (n.next != null) {
+                if (cur.data == n.next.data) {
+                    n.next = n.next.next;
+                } else {
+                    n = n.next;
+                }
+            }
+            cur = cur.next;
+        }
     }
 }
