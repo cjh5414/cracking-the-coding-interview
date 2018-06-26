f01d4d04ffd50e982c4da5fbd89f5a933a42ff2a
cjh5414 <cjh5414@gmail.com>
2018-06-25 23:48:45 -0700

    Update Q2_02 with correct answer

--- Ch02.Linked-Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
+++ Ch02.Linked-Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
@@ -7,10 +7,12 @@ public class QuestionB {
     public static int count = 0;
 
     public static Integer getKthToLast(LinkedListNode n, int p) {
-        if (n.next == null)
-            count = 1;
+        if (n == null)
+            count = 0;
         else {
-            getKthToLast(n.next, p);
+            Integer result = getKthToLast(n.next, p);
+            if (result != null)
+                return result;
             count++;
             if (count == p)
                 return n.data;
