5343bf48584ef4ec1367ac6919008c8de4d36d5c
cjh5414 <cjh5414@gmail.com>
2018-07-04 20:02:31 -0700

    Init Q2_06_Palindrome

---
+++ Ch02.Linked-Lists/Q2_06_Palindrome/QuestionA.java
@@ -0,0 +1,11 @@
+package Q2_06_Palindrome;
+
+import LinkedListsLibrary.LinkedListNode;
+
+
+public class QuestionA {
+    public static boolean isPalindrome(LinkedListNode n) {
+
+        return true;
+    }
+}
---
+++ Ch02.Linked-Lists/Q2_06_Palindrome/Tester.java
@@ -0,0 +1,15 @@
+package Q2_06_Palindrome;
+
+import LinkedListsLibrary.LinkedListNode;
+
+
+public class Tester {
+    public static void main(String[] args) {
+        int[] intlist = {0, 1, 2, 1, 0};
+
+        LinkedListNode head = new LinkedListNode(intlist);
+
+        System.out.println(head.printForward());
+        System.out.println(" Palindrome : " + QuestionA.isPalindrome(head));
+    }
+}
