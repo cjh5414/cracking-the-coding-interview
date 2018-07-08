b6d34db87b1a70f4c8c9f3589479c867b1dc0900
cjh5414 <cjh5414@gmail.com>
2018-07-07 18:02:45 -0700

    Add Q2_06 QuestionB with Runner

--- Ch02.Linked-Lists/Q2_06_Palindrome/QuestionB.java
+++ Ch02.Linked-Lists/Q2_06_Palindrome/QuestionB.java
@@ -1,10 +1,30 @@
 package Q2_06_Palindrome;
 
+import java.util.*;
+
 import LinkedListsLibrary.LinkedListNode;
 
 
 public class QuestionB {
     public static boolean isPalindrome(LinkedListNode head) {
-        return false;
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (fast != null && fast.next != null) {
+            stack.push(slow.data);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null)
+            slow = slow.next;
+
+        while (slow != null) {
+            if (slow.data != stack.pop()) return false;
+            slow = slow.next;
+        }
+
+        return true;
     }
 }
--- Ch02.Linked-Lists/Q2_06_Palindrome/Tester.java
+++ Ch02.Linked-Lists/Q2_06_Palindrome/Tester.java
@@ -5,12 +5,19 @@ import LinkedListsLibrary.LinkedListNode;
 
 public class Tester {
     public static void main(String[] args) {
-        int[] intlist = {0, 1, 2, 2, 0};
+        int[] intlist1 = {0, 1, 2, 2, 1, 0};
+        int[] intlist2 = {0, 1, 2, 2, 0};
 
-        LinkedListNode head = new LinkedListNode(intlist);
+        LinkedListNode head = new LinkedListNode(intlist1);
 
         System.out.println(head.printForward());
         System.out.println("A. Palindrome : " + QuestionA.isPalindrome(head));
         System.out.println("B. Palindrome : " + QuestionB.isPalindrome(head));
+
+        LinkedListNode head2 = new LinkedListNode(intlist2);
+
+        System.out.println(head2.printForward());
+        System.out.println("A. Palindrome : " + QuestionA.isPalindrome(head2));
+        System.out.println("B. Palindrome : " + QuestionB.isPalindrome(head2));
     }
 }
