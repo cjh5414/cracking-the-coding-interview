a43395fcf3a908ae71f606bad84021a8897d2849
cjh5414 <cjh5414@gmail.com>
2018-07-09 21:40:49 -0700

    Init Q2_08

---
+++ Ch02.Linked-Lists/Q2_08_Loop_Detection/QuestionA.java
@@ -0,0 +1,10 @@
+package Q2_08_Loop_Detection;
+
+import LinkedListsLibrary.LinkedListNode;
+
+
+public class QuestionA {
+    public static LinkedListNode getIntersection(LinkedListNode head1, LinkedListNode head2) {
+        return null;
+    }
+}
---
+++ Ch02.Linked-Lists/Q2_08_Loop_Detection/Tester.java
@@ -0,0 +1,29 @@
+package Q2_08_Loop_Detection;
+
+import LinkedListsLibrary.LinkedListNode;
+
+
+public class Tester {
+    public static void main(String[] args) {
+        int[] intlist = {1, 2, 3, 4, 5};
+
+        LinkedListNode head = new LinkedListNode(intlist);
+
+        LinkedListNode temp = null;
+        LinkedListNode node3 = null;
+
+        temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+            if (temp.data == 3)
+                node3 = temp;
+        }
+        temp.next = node3;
+
+        temp = head;
+        for (int i = 0; i < 6; i++) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+    }
+}
