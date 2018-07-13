3b3af6c262dcdcd374dde4fe8137e09fecd024c1
cjh5414 <cjh5414@gmail.com>
2018-07-12 20:58:33 -0700

    Correct Q2_08

--- Ch02.Linked-Lists/Q2_08_Loop_Detection/QuestionA.java
+++ Ch02.Linked-Lists/Q2_08_Loop_Detection/QuestionA.java
@@ -5,6 +5,24 @@ import LinkedListsLibrary.LinkedListNode;
 
 public class QuestionA {
     public static LinkedListNode findStartCircle(LinkedListNode head) {
-        return head;
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+
+        while (fast == null && fast.next != null) {
+            if (fast == slow) {
+                System.out.println("hi");
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return null;
     }
 }
