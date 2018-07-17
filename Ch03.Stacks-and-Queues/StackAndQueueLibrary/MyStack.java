6829e8cbeeb534d6a1e330d648d36dbbcbe8c4a3
cjh5414 <cjh5414@gmail.com>
2018-07-16 18:38:14 -0700

    Correct MyStack

--- Ch03.Stacks-and-Queues/StackAndQueueLibrary/MyStack.java
+++ Ch03.Stacks-and-Queues/StackAndQueueLibrary/MyStack.java
@@ -6,15 +6,19 @@ import java.util.Iterator;
 
 public class MyStack<T> {
     private static class StackNode<T> {
-        T data;
+        private T data;
         private StackNode<T> next;
 
         public StackNode(T data) {
             this.data = data;
         }
+
+        public T getData() {
+            return this.data;
+        }
     }
 
-    private StackNode head = null;
+    private StackNode<T> head = null;
 
     public void push(T data) {
         StackNode<T> node = new StackNode<T>(data);
@@ -25,15 +29,23 @@ public class MyStack<T> {
     public T pop() {
         StackNode<T> temp = head;
         head = head.next;
-        return temp.data;
+        return temp.getData();
+    }
+
+    public T peek() {
+        return head.getData();
+    }
+
+    public boolean isEmpty() {
+        return head == null;
     }
 
     public void print() {
         StackNode<T> temp = head;
         while (temp != null) {
-            System.out.println(temp.data + " ");
+            System.out.print(temp.getData() + " ");
             temp = temp.next;
         }
+        System.out.println();
     }
 }
-
--- Ch03.Stacks-and-Queues/StackAndQueueLibrary/Tester.java
+++ Ch03.Stacks-and-Queues/StackAndQueueLibrary/Tester.java
@@ -4,14 +4,18 @@ public class Tester {
     public static void main(String[] args) {
         MyStack<Integer> myStack = new MyStack<Integer>();
 
+        System.out.println("Origin : ");
         myStack.push(10);
         myStack.push(32);
         myStack.push(5);
-
         myStack.print();
 
-        myStack.pop();
-
+        System.out.println("Pop " + myStack.pop() + " : ");
         myStack.print();
+
+        System.out.println("Peek " + myStack.peek() + " : ");
+        myStack.print();
+
+        System.out.println("IsEmpty : " + myStack.isEmpty());
     }
 }
