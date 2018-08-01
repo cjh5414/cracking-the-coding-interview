6ad8d670da8e5530ee4e68220c5406e856fdc127
cjh5414 <cjh5414@gmail.com>
2018-07-31 23:26:12 -0700

    Init Q3_04

---
+++ Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/MyQueue.java
@@ -0,0 +1,6 @@
+package Q3_04_Queue_via_Stacks;
+
+
+class MyQueue {
+
+}
---
+++ Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/Tester.java
@@ -0,0 +1,14 @@
+package Q3_04_Queue_via_Stacks;
+
+
+public class Tester {
+    public static void main(String[] args) {
+        MyQue
+
+        for (int i = 0; i < 36; i++)
+            setOfStacks.push(i);
+
+        for (int i = 0; i < 37; i++)
+            System.out.println("Popped " + setOfStacks.pop());
+    }
+}
