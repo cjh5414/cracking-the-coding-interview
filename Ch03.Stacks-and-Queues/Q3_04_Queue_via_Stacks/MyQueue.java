06ef315b363112a070b6f881b8743a48fdea3fc1
cjh5414 <cjh5414@gmail.com>
2018-08-02 22:19:15 -0700

    Create Q3_04

--- Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/MyQueue.java
+++ Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/MyQueue.java
@@ -1,9 +1,39 @@
 package Q3_04_Queue_via_Stacks;
 
+import java.util.Stack;
 
-class MyQueue {
-    public void push(int value) {
+class MyQueue<T> {
+    Stack<T> newStack = new Stack<T>();
+    Stack<T> oldStack = new Stack<T>();
 
+    public void push(T value) {
+        newStack.push(value);
+    }
 
+    public T pop() {
+        if (oldStack.empty()) {
+            int newStackSize = newStack.size();
+            for (int i = 0; i < newStackSize - 1; i++) {
+                oldStack.push(newStack.pop());
+            }
+            return newStack.pop();
+        }
+        else {
+            return oldStack.pop();
+        }
+    }
+
+    public T peek() {
+        if (oldStack.empty()) {
+            int newStackSize = newStack.size();
+
+            for (int i = 0; i < newStackSize; i++) {
+                oldStack.push(newStack.pop());
+            }
+            return oldStack.peek();
+        }
+        else {
+            return oldStack.peek();
+        }
     }
 }
--- Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/Tester.java
+++ Ch03.Stacks-and-Queues/Q3_04_Queue_via_Stacks/Tester.java
@@ -3,16 +3,29 @@ package Q3_04_Queue_via_Stacks;
 
 public class Tester {
     public static void main(String[] args) {
-        MyQueue myQueue = new MyQueue();
+        MyQueue<Integer> myQueue = new MyQueue<Integer>();
 
         myQueue.push(2);
+        System.out.println("Peek: " + myQueue.peek());
         myQueue.push(4);
+        System.out.println("Peek: " + myQueue.peek());
         myQueue.push(10);
 
-        myQueue.print();
+        System.out.println("Pop: " + myQueue.pop());
+        System.out.println("Pop: " + myQueue.pop());
+        System.out.println("Pop: " + myQueue.pop());
+
+        myQueue.push(3);
+        myQueue.push(7);
+
+        System.out.println("Peek: " + myQueue.peek());
+        System.out.println("Pop: " + myQueue.pop());
+        System.out.println("Peek: " + myQueue.peek());
+
+        myQueue.push(9);
 
         System.out.println("Pop: " + myQueue.pop());
         System.out.println("Pop: " + myQueue.pop());
-        System.out.println("Pop: " + myQueue.pop());
+
     }
 }
