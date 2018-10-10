fe0aaf108024db0d68e6942a1b2aa1708a10f39e
cjh5414 <cjh5414@gmail.com>
2018-10-10 19:46:01 +0900

    Add Q4_03 main

---
+++ Ch04.Trees-and-Graphs/Q4_03_List_of_Depths/Main.java
@@ -0,0 +1,14 @@
+package Q4_03_List_of_Depths;
+
+/**
+ * Created by jihun on 2018. 10. 10..
+ */
+public class Main {
+    public static void main(String[] args) {
+        QuestionBFS qBFS = new QuestionBFS();
+        qBFSq.main();
+
+        QuestionDFS qDFS = new QuestionBFS();
+        qDFSq.main();
+    }
+}
